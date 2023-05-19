7ef9135b0300c6ad69e85f36c5b0dbde94862953
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-05-19 14:48:18 +0000

    Update functions

--- helper_func.h
+++ helper_func.h
@@ -13,9 +13,15 @@ ssize_t _getline(char **buff, size_t *num, int fildes);
 size_t str_len(char *s);
 char *str_cpy(char *des, char *src);
 char *str_dup(char *s);
+char *_strcat(char *dest, const char *src);
 void child(char *route, char *args[], char **ens);
 int print_en(void);
+void prompt(void);
 int str_cmp(char *s1, char *s2);
-int check_cmd(char *instruct, char **cmds, char *prognm);
+void start_child(char **instruct);
+int check_cmd(char **cmds);
+int cdry(char *cmds);
+char *_getenv(const char *name);
+size_t numstr(char *str, char sep);
 char **string_split(char *str, char *sep, ssize_t len);
 #endif /* HELPER_FUNC_H */
