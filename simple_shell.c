b95d1ac33795a2997510f738e6db68ad158738e2
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-05-26 22:02:51 +0000

    Update Functions

--- simple_shell.c
+++ simple_shell.c
@@ -24,8 +24,8 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 		nread = _getline(buff, &num, STDIN_FILENO);
 		if (nread == 1 && buff[0] == '\n')
 			continue;
-		/* if (nread <= 0)
-			exit(0);*/
+		if (nread <= 0)
+			exit(0);
 
 		if (cdry(buff) == 0)
 			continue;
